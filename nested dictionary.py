3bfb382d4be98bad752a3a3fa172cd347fc2e7a0
Vyshnavmv <110896669+Vyshnavmv@users.noreply.github.com>
2022-08-09 15:03:49 +0530

    first commit

---
+++ nested dictionary.py
@@ -0,0 +1,22 @@
+nested_dictionary={
+    "one":
+    {
+     "name":"john",
+     "age":30,
+     "city":"newyork"
+},
+     "two":
+{
+     "name":"vyshnav",
+     "age":25,
+     "city":"paris"
+},
+     "three":
+{
+     "name":"messi",
+     "age":20,
+     "city":"argentina"
+
+}}
+
+print(nested_dictionary["three"]["name"])    #extract
